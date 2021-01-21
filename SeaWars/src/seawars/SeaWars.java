2a92984441737c2ea5a3235de11ec2afb7ff956a
Daniel Bejarano <dbejarano820@gmail.com>
2021-01-20 20:36:24 -0600

    Prueba

---
+++ SeaWars/src/seawars/SeaWars.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seawars;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class SeaWars {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("En esta progra vamos a sacar 100");
+        
+        
+    }
+    
+}
