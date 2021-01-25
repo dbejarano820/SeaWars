a496abfe9e4d09f672e046fcc4c7db04cf1952f8
JosueGupi <josuegupi@gmail.com>
2021-01-24 22:33:00 -0600

    Poseidon trident

--- SeaWars/src/Game/Superpower.java
+++ SeaWars/src/Game/Superpower.java
@@ -23,23 +23,199 @@ public interface Superpower {
 
 class TheTrident implements Superpower{
     
-    @Override
-    public String attack1(Player player,Player atacante){
+    //revision
+    public String attack1(Player player,Player atacante, int x1, int x2, int x3, int y1, int y2, int y3){//three lines
         String res = "";
+        atacante.cantidadAtaques++;
+        int cantCasillas = new Random().nextInt(4)+1;
+        int direccion = new Random().nextInt(4);
+        
+        if(direccion == 0){
+            for(int i = 0; i<cantCasillas; i++){
+                if(y1 < 0)
+                    break;
+                if(player.tablero[y1][x1].vida > 0){
+                    player.tablero[y1][x1].vida = 0;
+                    player.tablero[y1][x1].historial += "Fue destruido por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                    res += "La casilla("+x1+","+y1+") fue destruida por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                }
+                y1--;
+            }
+            
+            for(int i = 0; i<cantCasillas; i++){
+                if(y2 < 0)
+                    break;
+                if(player.tablero[y2][x2].vida > 0){
+                    player.tablero[y2][x2].vida = 0;
+                    player.tablero[y2][x2].historial += "Fue destruido por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                    res += "La casilla("+x2+","+y2+") fue destruida por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                }
+                y2--;
+            }
+            
+            for(int i = 0; i<cantCasillas; i++){
+                if(y3 < 0)
+                    break;
+                if(player.tablero[y3][x3].vida > 0){
+                    player.tablero[y3][x3].vida = 0;
+                    player.tablero[y3][x3].historial += "Fue destruido por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                    res += "La casilla("+x3+","+y3+") fue destruida por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                }
+                y3--;
+            }
+            
+        }
+        else if(direccion == 1){
+            for(int i = 0; i<cantCasillas; i++){
+                if(x1 < 0)
+                    break;
+                if(player.tablero[y1][x1].vida > 0){
+                    player.tablero[y1][x1].vida = 0;
+                    player.tablero[y1][x1].historial += "Fue destruido por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                    res += "La casilla("+x1+","+y1+") fue destruida por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                }
+                x1--;
+            }
+            
+            for(int i = 0; i<cantCasillas; i++){
+                if(x2 < 0)
+                    break;
+                if(player.tablero[y2][x2].vida > 0){
+                    player.tablero[y2][x2].vida = 0;
+                    player.tablero[y2][x2].historial += "Fue destruido por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                    res += "La casilla("+x2+","+y2+") fue destruida por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                }
+                x2--;
+            }
+            
+            for(int i = 0; i<cantCasillas; i++){
+                if(x3 < 0)
+                    break;
+                if(player.tablero[y3][x3].vida > 0){
+                    player.tablero[y3][x3].vida = 0;
+                    player.tablero[y3][x3].historial += "Fue destruido por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                    res += "La casilla("+x3+","+y3+") fue destruida por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                }
+                x3--;
+            }
+            
+        }
+        
+        else if(direccion == 2){
+            for(int i = 0; i<cantCasillas; i++){
+                if(y1 > 19)
+                    break;
+                if(player.tablero[y1][x1].vida > 0){
+                    player.tablero[y1][x1].vida = 0;
+                    player.tablero[y1][x1].historial += "Fue destruido por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                    res += "La casilla("+x1+","+y1+") fue destruida por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                }
+                y1++;
+            }
+            for(int i = 0; i<cantCasillas; i++){
+                if(y3 > 19)
+                    break;
+                if(player.tablero[y3][x3].vida > 0){
+                    player.tablero[y3][x3].vida = 0;
+                    player.tablero[y3][x3].historial += "Fue destruido por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                    res += "La casilla("+x3+","+y3+") fue destruida por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                }
+                y3++;
+            }
+            for(int i = 0; i<cantCasillas; i++){
+                if(y2 > 19)
+                    break;
+                if(player.tablero[y2][x2].vida > 0){
+                    player.tablero[y2][x2].vida = 0;
+                    player.tablero[y2][x2].historial += "Fue destruido por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                    res += "La casilla("+x2+","+y2+") fue destruida por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                }
+                y2++;
+            }
+            
+        }
+        
+        else if(direccion == 3){
+            for(int i = 0; i<cantCasillas; i++){
+                if(x1 > 29)
+                    break;
+                if(player.tablero[y1][x1].vida > 0){
+                    player.tablero[y1][x1].vida = 0;
+                    player.tablero[y1][x1].historial += "Fue destruido por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                    res += "La casilla("+x1+","+y1+") fue destruida por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                }
+                x1++;
+            }
+            for(int i = 0; i<cantCasillas; i++){
+                if(x2 > 29)
+                    break;
+                if(player.tablero[y2][x2].vida > 0){
+                    player.tablero[y2][x2].vida = 0;
+                    player.tablero[y2][x2].historial += "Fue destruido por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                    res += "La casilla("+x2+","+y2+") fue destruida por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                }
+                x2++;
+            }
+            for(int i = 0; i<cantCasillas; i++){
+                if(x3 > 29)
+                    break;
+                if(player.tablero[y3][x3].vida > 0){
+                    player.tablero[y3][x3].vida = 0;
+                    player.tablero[y3][x3].historial += "Fue destruido por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                    res += "La casilla("+x3+","+y3+") fue destruida por un tridente de poseidon enviado por "+atacante.nombre+"\n";
+                }
+                x3++;
+            }
+            
+        }
+        
+        if(!res.equals(""))
+            atacante.cantidadAtinados++;
         
         return res;
     }
-    @Override
-    public String attack2(Player player,Player atacante){
+    
+    //tocar los casos de los threads
+    
+    public String attack2(Player player,Player atacante, int cantDestrucciones){
         String res = "";
+        int x= 0;
+        int y = 0;
+        int i = 0;
+        while(i < cantDestrucciones){
+            x = new Random().nextInt(29);
+            y = new Random().nextInt(19);
+            if(!player.estaVivo())
+                break;
+            if(player.tablero[y][x].vida > 0){
+                player.tablero[y][x].vida = 0;
+                player.tablero[y][x].historial += "Fue destruida por el Three Numbers enviado por "+atacante.nombre;
+                res += "La casilla("+x+","+y+") fue destruida por el Three Numbers enviado por "+atacante.nombre;
+            }
+        }
         
         return res;
     }
+    //revision
     @Override
-    public String attack3(Player player,Player atacante){
+    public String attack3(Player player,Player atacante){//control the kraken
         String res = "";
-        
+        atacante.krakenDefense = true;
+        res += atacante.nombre+" se protegera del siguiente ataque del Kraken en esta ronda";
+        atacante.cantidadAtaques++;
+        atacante.cantidadAtinados++;
         return res;
+    }
+    
+    // no se usa
+    @Override
+    public String attack1(Player player, Player atacante) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    // no se usa
+    @Override
+    public String attack2(Player player, Player atacante) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
 
@@ -67,6 +243,7 @@ class ThundersUnderTheSea implements Superpower{
     
 class ReleaseTheKraken implements Superpower{
     
+    //revision
     @Override
     public String attack1(Player player,Player atacante){//Tentaculos
         atacante.cantidadAtaques++;
@@ -204,7 +381,7 @@ class ReleaseTheKraken implements Superpower{
     }
     
     
-    
+    //revision
     public String attack2(Player player,Player atacante, int x, int y){//Kraken breath
         String res = "";
         int direccion = new Random().nextInt(4);
@@ -277,9 +454,15 @@ class ReleaseTheKraken implements Superpower{
         return res;
     }
     
-    
+    //revision
     @Override
     public String attack3(Player player,Player atacante){//Release the kraken
+        
+        if(player.krakenDefense){
+            player.krakenDefense = false;
+            attack3(atacante,player);
+        }
+        
         String res = "";
         int casillaX = new Random().nextInt(30);
         int casillaY = new Random().nextInt(20);
@@ -311,7 +494,7 @@ class ReleaseTheKraken implements Superpower{
     public String attack2(Player player, Player atacante) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-}   
+}
 
 class WavesControl implements Superpower{
     
